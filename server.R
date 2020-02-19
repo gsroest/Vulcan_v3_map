9c0a37f76757040d92fec9a4f373923dc035fff5
gsroest <53795128+gsroest@users.noreply.github.com>
2020-02-19 14:38:36 -0700

    using map saved as RData file

--- server.R
+++ server.R
@@ -5,36 +5,10 @@ library(raster)
 
 shinyServer(function(input, output) {
   
-  #r <- raster('./data/total.mn.2011.tif')
-  #crs(r) <- sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-  #r <- log(r)
+  load('./vulcan.map.RData')
   
-  # Create enlarged city boundaries for map
-  #bounds <- extent(r)
-  #lng1   <- extent(r)[1]-5
-  #lng2   <- extent(r)[2]+5
-  #lat1   <- extent(r)[3]-5
-  #lat2   <- extent(r)[4]+5
-  
-  #vals <- getValues(r)
-  #vals <- vals[which(!is.na(vals) & !is.infinite(vals) & vals > -10)]
-  #colors   <- colorRampPalette(c("navy","blue","green3","yellow","orange","darkred"))
-  colors   <- colorRampPalette(c("navy","blue","white","red","darkred"))
-  pal <- colorNumeric(colors(101), domain=vals,
-  #                    na.color = "transparent")
-
   # create the leaflet map  
   output$vulcan_map <- renderLeaflet({
-    leaflet() %>% 
-    addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
-    addTiles(urlTemplate = 'https://github.com/gsroest/Vulcan_v3_map/tree/master/data/tiles/total/{z}/{x}/{y}.png',
-             group="Total", 
-             options=tileOptions(noWrap=T, opacity=0.5, minZoom=0, maxZoom=10, tms = T)) %>%
-    addLayersControl(baseGroups=c("ESRI Grey Canvas"), overlayGroups="Total")
-    #addRasterImage(r, colors=pal, project = T, opacity=0.5,
-    #               maxBytes=16*38595600) %>%
-    #addLegend(pal = pal, values = vals,
-    #          title = "log(tC")
-    
+    vulcan.map  
   })
 })
